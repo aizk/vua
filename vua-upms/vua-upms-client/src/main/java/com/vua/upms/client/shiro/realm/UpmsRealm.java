b600575014133cc127872c9845761eecd53d6088
liunian <78435431@qq.com>
2017-06-26 21:48:45 +0800

    授权

--- vua-upms/vua-upms-client/src/main/java/com/vua/upms/client/shiro/realm/UpmsRealm.java
+++ vua-upms/vua-upms-client/src/main/java/com/vua/upms/client/shiro/realm/UpmsRealm.java
@@ -1,14 +1,24 @@
 package com.vua.upms.client.shiro.realm;
 
+import com.vua.upms.dao.model.UpmsPermission;
+import com.vua.upms.dao.model.UpmsRole;
+import com.vua.upms.dao.model.UpmsUser;
+import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.vua.upms.rpc.api.UpmsApiService;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * 认证 授权
@@ -25,8 +35,28 @@ public class UpmsRealm extends AuthorizingRealm {
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         String username = (String)principalCollection.getPrimaryPrincipal();
+        UpmsUser upmsUser = upmsApiService.selectUpmsUserByUsername(username);
+        List<UpmsRole> upmsRoles = upmsApiService.selectUpmsRoleByUpmsUserId(upmsUser.getUserId());
+        List<UpmsPermission> upmsPermissions = upmsApiService.selectUpmsPermissionByUpmsUserId(upmsUser.getUserId());
 
-        return null;
+        Set<String> roles = new HashSet<>();
+        for (UpmsRole upmsRole : upmsRoles) {
+            if (StringUtils.isNotBlank(upmsRole.getName())) {
+                roles.add(upmsRole.getName());
+            }
+        }
+
+        Set<String> permissions = new HashSet<>();
+        for (UpmsPermission upmsPermission : upmsPermissions) {
+            if (StringUtils.isNotBlank(upmsPermission.getPermissionValue())) {
+                permissions.add(upmsPermission.getPermissionValue());
+            }
+        }
+
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        simpleAuthorizationInfo.setRoles(roles);
+        simpleAuthorizationInfo.setStringPermissions(permissions);
+        return simpleAuthorizationInfo;
     }
 
     //认证
